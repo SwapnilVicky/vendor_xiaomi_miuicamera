12060eab1b507247c87d031b4674aac9761808bf
Frost <frost.github@proton.me>
2026-03-02 15:30:53 +0530

    miuicamera-miatoll: Don't explicitly build `libgui_shim_miuicamera`
    Change-Id: I019a168142cf14bc5496ccbf319142d4db90b974
    Signed-off-by: Frost <frost.github@proton.me>

--- MiuiCamera.mk
+++ MiuiCamera.mk
@@ -19,10 +19,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-miui.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miui.xml
 
-# Shims
-PRODUCT_PACKAGES += \
-    libgui_shim_miuicamera
-
 # Sysconfig
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
